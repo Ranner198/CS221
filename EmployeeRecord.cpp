e44e6eef8230934aae8c2b8de6f3f543e890fdb0
Ran Crump <rancrump18@gmail.com>
2018-06-16 17:53:33 -0500

    V0.01
    +Skeleton Started
    +Simple Class 
    +.h & .cpp

---
+++ EmployeeRecord.cpp
@@ -0,0 +1,51 @@
+//
+//  EmployeeRecord.cpp
+//  C++Homework
+//
+//  Created by Ran Crump  on 6/15/18.
+//
+#include <iostream>
+#include <iomanip>
+#include "EmployeeRecord.h"
+
+using namespace std;
+
+void EmployeeRecord::getName(char* fName, char* lName) {
+	//Not sure here?
+};
+
+int main() {
+    
+	int _ID = 0;
+
+	double _salary = 0.0;
+
+	char first[32] = "", last[32] = "";
+
+	//ID Test
+	cout << "Enter Employee ID Number:";
+	cin >> _ID;
+
+	//Salary Test
+	cout << "Enter Employee Salary Number:";
+	cin >> _salary;
+
+	//Name Test
+	cout << "Enter Name:";
+	cin >> first >> last;
+
+	EmployeeRecord _EmployeeRecord;
+    
+	_EmployeeRecord.setID(_ID);
+
+	_EmployeeRecord.getName(first, last);
+
+	_EmployeeRecord.setSalary(_salary);
+
+	_EmployeeRecord.PrintRecord();
+
+	_EmployeeRecord.~EmployeeRecord();
+
+	system("pause");
+    return 0;
+}
---
+++ EmployeeRecord.h
@@ -0,0 +1,66 @@
+//
+//  EmployeeRecord.h
+//  C++Homework
+//
+//  Created by Ran Crump  on 6/15/18.
+//
+#pragma once
+
+class EmployeeRecord
+{
+private:
+    int m_iEmployeeID;
+    char m_sLastName[32];
+    char m_sFirstName[32];
+    int m_iDeptID;
+    double m_dSalary;
+    
+public:
+    //Default Construtor
+	EmployeeRecord() {
+		m_iEmployeeID = 0;
+		strcpy_s(m_sLastName, "");
+		strcpy_s(m_sFirstName, "");
+		m_iDeptID = 0;
+		m_dSalary = 0.0;
+	};
+
+	//Construtor Function
+	EmployeeRecord(int ID, char *fName, char *lName, int dept, double sal) {
+	};
+
+	//Default Deconstructor
+    ~EmployeeRecord(){
+	}
+
+    //Employee ID
+	void setID(int ID) {
+		m_iEmployeeID = ID;
+	};
+	int getID() {
+		return m_iEmployeeID;
+	};
+    
+	//Employee Name
+	void setName(char* fName, char* lName) {
+		strcpy_s(m_sFirstName, fName);
+		strcpy_s(m_sLastName, lName);
+	};
+	void getName(char* fName, char* lName);
+
+
+    //Employee Salary
+	void setSalary(double sal) {
+		m_dSalary = sal;
+	};
+	void getSalary(double* sal) {
+		setSalary(*sal);
+	};
+
+    
+    //Print Employee Record to screen
+	void PrintRecord() {
+		std::cout << "Employee ID: " << m_iEmployeeID << std::endl;
+		std::cout << "Salary: " << m_dSalary << std::endl;
+	};
+};
